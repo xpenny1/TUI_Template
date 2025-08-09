4ff6ad8f88c3ffb4a161da9dc072f1e394aef343
Can Ward <can.ward.99@gmail.com>
2025-08-09 15:49:03 +0200

    read key

--- template.sh
+++ template.sh
@@ -2,7 +2,9 @@
 
 main() {
     alternateScreenBuffer -e
-    c=$(choose a b c)
+    echo reading Key
+    c="$(readKey)"
+    echo reading "done"
     alternateScreenBuffer -q
     echo $c
 }
@@ -11,6 +13,7 @@ err() {
     echo $1 >&2
     exit 1
 }
+
 unhandled() {
     err "Das Argument $1 wird in $2 nich korrekt behandelt!"
 }
@@ -27,38 +30,51 @@ readCursor() {
         esac
     done
     printf "\e[6n"
-    read -s -n 2
-    IFS=';' read -s -d R -a $varName
+    read -srn 2
+    IFS=';' read -sr -d R -a $varName
 }
 
 alternateScreenBuffer() {
     OPTIND=1
     while getopts "esdq" arg; do
         case $arg in
-            "e"|"s") printf "\e[?1049h";;
-            "d"|"q") printf "\e[?1049l";;
+            "e"|"s") printf "\e[?1049h"   ;;
+            "d"|"q") printf "\e[?1049l"   ;;
             *) unhandled "$arg" readCursor;;
         esac
     done
 }
-readArrow() {
-    while read -rsn 1 key; do
-        case $key in
-            A) echo Up;    break;;
-            B) echo Down;  break;;
-            C) echo Right; break;;
-            D) echo Left;  break;;
-            *) ;;
-        esac
-    done
+readKey() {
+    local key
+    read -rsn 1 key
+    case $key in
+        [a-z,A-Z,0-9]) echo $key   ;;
+                $'\e') readEscape  ;;
+                    *) echo Unknown;;
+    esac    
 }
+readEscape() {
+    echo Escape
+    local key
+    read -rsn 1 key
+    [[ $key != "[" ]] && { echo Unknown; return 1; }
+    read -rsn 1 key
+    case $key in
+            A) echo Up                    ;;
+            B) echo Down                  ;;
+            C) echo Right                 ;;
+            D) echo Left                  ;;
+            *) echo Unknown $key; return 1;;
+    esac    
+}
+
 
 choose() {
     local s=1
     while true; do
         local i=1
         for arg in "$@"; do
-            printf "\e[""$i"";H" >&2
+            printf '\e[%s;H' "$i" >&2
             [[ "$i" == "$s" ]] && echo -e "\e[38;5;212m""$arg\e[0m" >&2 \
                                || echo "$arg" >&2
             i=$((i+1))
